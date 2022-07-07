a0730da0e626dc53202dbf18df694a0715296e82
Ramin Javi <ramin.javvi@gmail.com>
2022-07-07 18:03:27 +0430

    Add methods: getQuantity + findByUserId

--- app/Models/Gem.php
+++ app/Models/Gem.php
@@ -12,4 +12,26 @@ class Gem extends Model
      * @var array<int, string>
      */
     protected $fillable = ['user_id', 'quantity'];
+
+
+    /**
+     * Get User's total gems
+     *
+     * @param int $userId
+     * @return int $totalUserGems
+     */
+    public static function getQuantity(int $userId): int
+    {
+        $gem = self::where('user_id', $userId)->first();
+        return $gem ? $gem->quantity : 0;
+    }
+
+    /**
+     * @param int $userId
+     * @return Model $gem
+     */
+    public static function findByUserId(int $userId): Model
+    {
+        return self::where('user_id', $userId)->first();
+    }
 }
